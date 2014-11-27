8b6d8a2a28922e5587ac4f8126a396dd56acab79
navazish <navazish@ubuntu.(none)>
2014-11-27 14:31:55 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaypointsExample::Application.config.secret_token = 'cac42d19238a3e55ad0c0276cf65571e1a161a9ec0fa22b45b3d109bafddad7938d8f13611c8de132fa6cac2c666c9c59bc02e04c40504806abbb211f8de019c'
